8c3f188d450dc5ee66a2da3e5a8f876e96beee15
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2019-11-05 22:02:24 -0500

    completed work, may attempt stretch

--- index.js
+++ index.js
@@ -98,6 +98,8 @@ function Baby(name, age, favoriteToy) {
   this.favoriteToy= favoriteToy;
 }
 
+Baby.prototype= Object.create(Person.prototype);
+
 Baby.prototype.play= function(){
   return `Playing with ${this.favoriteToy}`;
 }
